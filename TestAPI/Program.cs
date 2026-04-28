324cf0c6ac837244093235f2a60b396233d2aee3
charanbaghel-developer <charan.baghel@gmail.com>
2026-04-28 18:30:37 +0530

    Update Program.cs

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -36,11 +36,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Swagger
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // ❌ REMOVE THIS (important for Render)
 // app.UseHttpsRedirection();
